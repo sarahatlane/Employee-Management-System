77c371b3f8965cb7035473017b3d64606ce490fd
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-10-29 21:32:58 +0200

    ../

--- Frontend/src/app/ app-routing.module.ts
+++ Frontend/src/app/ app-routing.module.ts
@@ -1,13 +1,17 @@
+// AppRoutingModule ensure that routing is set up and ready to handle navigation within the application.
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
     // ...other routes
-    { path: '', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
   ];
 
+  // decorator used to define and configure an Angular module
   @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule],
