2daa9f5d9f76c162aeffa070537c8bbe658cc959
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-02 11:07:25 +0200

    ../

--- backend/employeemanagementsystem/employeemanagementsystem/urls.py
+++ backend/employeemanagementsystem/employeemanagementsystem/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projectname project.
+URL configuration for employeemanagementsystem project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
