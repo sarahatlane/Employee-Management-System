7e4cec5f40cef7802652cdba2e28213f05af059c
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-03 22:50:05 +0200

    ../

--- backend/test_employee/settings.py
+++ backend/test_employee/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'test_employee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+         'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'employees',
+        'USER': 'Sara',
+        'PASSWORD': 'Sara@0720',
+        'HOST': 'localhost',  # or your MySQL host
+        'PORT': '3306',
     }
 }
 
