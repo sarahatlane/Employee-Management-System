1fe23985d090c85d4a2cd5b46f4bdf98a2dfa7f4
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-06 00:05:31 +0200

    ../

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'employees',
+        'USER': 'root',        
+        'PASSWORD': '',  
+        'HOST': 'localhost',           
+        'PORT': '4306', 
     }
 }
 
