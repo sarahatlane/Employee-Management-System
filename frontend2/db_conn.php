def29c740837c26db7725392b3a6fc58d57f5892
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-06 20:43:57 +0200

    ./

--- frontend2/db_conn.php
+++ frontend2/db_conn.php
@@ -1,7 +1,7 @@
 <!-- Admin user -->
 
 <?php
-$servername = "localhost";
+$servername = "localhost:3306";
 $username = "root";
 $password = "";
 $dbname = "php-employees";
@@ -10,7 +10,14 @@ $dbname = "php-employees";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
 }
+?>
+// if (!$conn) {
+//   die("Connection failed: " . mysqli_connect_error());
+// }
 // echo "Connected successfully";
+
