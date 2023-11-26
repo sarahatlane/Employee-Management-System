0b34cea604d605f53e50779f7d4916e839efc1df
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-26 22:49:39 +0200

    styling the cards/

--- Frontend/src/app/ app-routing.module.ts
+++ Frontend/src/app/ app-routing.module.ts
@@ -9,6 +9,7 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { AddComponent } from './components/add/add.component';
 import { ViewComponent } from './components/view/view.component';
 import { EditComponent } from './components/edit/edit.component';
+import { ReportComponent } from './components/report/report.component';
 
 //rounting path
 const routes: Routes = [
@@ -19,6 +20,7 @@ const routes: Routes = [
   { path: 'add', component: AddComponent },
   { path: 'view', component: ViewComponent },
   { path: 'edit', component: EditComponent },
+  { path: 'report', component: ReportComponent},
 ];
 
 @NgModule({
