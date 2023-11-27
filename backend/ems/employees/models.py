568ee2116901ee3cd28661b85702bfd1ad9048c1
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-27 11:45:12 +0200

    created employees models

--- backend/ems/employees/models.py
+++ backend/ems/employees/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    # Defined fields for the Employee model
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    department = models.CharField(max_length=100)
+    hire_date = models.DateField()
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    # Optional: Defined any additional methods or meta information
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
