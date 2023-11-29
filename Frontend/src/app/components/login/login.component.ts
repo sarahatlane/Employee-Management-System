722b4371e55bf9f08f1b379847805fb46a806f63
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-29 21:46:59 +0200

    ../working on login

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -13,6 +13,21 @@ export class LoginComponent {
   };
 
   constructor(private authService: AuthService) { }
+  onSubmit(): void {
+    this.authService.login(this.credentials).subscribe(
+      (response) => {
+        // Assuming the backend sends back a token upon successful login
+        const token = response.token; // Adjust this based on the actual response structure
+        // Store the token securely (e.g., in localStorage)
+        localStorage.setItem('token', token);
+        console.log('Login successful! Token stored:', token);
+      },
+      (error) => {
+        console.error('Login failed:', error);
+        // Handle login error here
+      }
+    );
+  }
 
 
 }
