fab5659ee138a804707238fd6e159e4fb73984c2
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-29 22:12:21 +0200

    ../working on login

--- backend/ems/employees/urls.py
+++ backend/ems/employees/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
-
+from .views import CustomTokenObtainPairView
 
 
 urlpatterns = [
     path('employees/', views.EmployeeListCreateView.as_view(), name='employee-list-create'),
     path('employees/<int:pk>/', views.EmployeeDetailView.as_view(), name='employee-detail'),
-    path('employee/', include('employees.urls')), 
+    path('employee/', include('employees.urls')),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+   
 ]
