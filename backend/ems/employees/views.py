0a22ef16727230b6cb28dfb8bb78ea7155bdb20a
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-29 22:10:42 +0200

    ../working on login

--- backend/ems/employees/views.py
+++ backend/ems/employees/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Employee
 from .serializers import EmployeeSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 
 # Create your views here.
@@ -13,3 +15,7 @@ class EmployeeListCreateView(generics.ListCreateAPIView):
 class EmployeeDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer  # Define your custom serializer here
