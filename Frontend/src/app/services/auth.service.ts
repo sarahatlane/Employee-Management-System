c90ccadd96b474cb721ae42fc4fbf9c93075f82e
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-29 21:30:21 +0200

    ../working on login

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private loginUrl = 'http://your-backend-url/login'; // Replace with your actual login endpoint URL
+
 
   constructor() { }
 }
