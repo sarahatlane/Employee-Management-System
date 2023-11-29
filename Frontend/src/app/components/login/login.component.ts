f93bc49ef72a6e65e2af6efea5701b2906ea01c1
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-29 21:44:43 +0200

    ../working on login

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AuthService } from '.../authService';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
